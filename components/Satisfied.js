5749a98b2074f10db10b9df6cfdf417122389061
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-18 15:51:15 +0100

    suprememods

--- components/Satisfied.js
+++ components/Satisfied.js
@@ -1,17 +1,18 @@
 export default function Satisfied() {
     return(
-        <div className="w-full px-4 lg:px-20 py-4 lg:py-8 sm:p-8 md:px-12 flex justify-center items-center space-x-[150px]">
-            <div className="max-w-[220px]">
+        <div className="w-full px-4 lg:px-20 py-4 lg:py-8 sm:p-8 md:px-12 flex flex-col md:flex-row
+         justify-center items-center md:space-x-[50px] lg:space-x-[75px] xl:space-x-[150px] space-y-5 md:space-y-0">
+            <div className="max-w-[220px] text-center md:text-start">
                 <h1 className="font-bold h1 text-pink">80+</h1>
                 <h6 className="h6">Satisfied clients</h6>
             </div>
-            <div className="h-28 bg-black w-0.5" />
-            <div className="max-w-[220px]">
+            <div className="h-0.5 w-[97px] md:h-28 bg-black md:w-0.5" />
+            <div className="max-w-[220px] text-center md:text-start">
                 <h1 className="font-bold h1 text-pink">200+</h1>
                 <h6 className="h6">Projects completed</h6>
             </div>
-            <div className="h-28 bg-black w-0.5" />
-            <div className="max-w-[220px]">
+            <div className="h-0.5 w-[97px] md:h-28 bg-black md:w-0.5" />
+            <div className="max-w-[220px] text-center md:text-start">
                 <h1 className="font-bold h1 text-pink">99+</h1>
                 <h6 className="h6">Reviews given</h6>
             </div>
