e5564a9cc326477a6a41e1a0fec32eb179b9190e
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-18 18:10:44 +0100

    suprememods

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -6,17 +6,18 @@ export default function Portfolio() {
             <div className="lg:w-[700px] xl:w-[500px] text-center space-y-2">
                 <h6 className="font-semibold text-pink h6">| Lorem ipsum |</h6>
                 <h1 className="font-bold text-ink h1">Our Projects</h1>
-                <p className="p px-5">Lorem ipsum dolor sit amet consectetur. Dui dictumst turpis tellus integer vulputate.</p>
+                <p className="p px-2.5 sm:px-10 lg:px-5">Lorem ipsum dolor sit amet consectetur. Dui dictumst turpis tellus integer vulputate.</p>
             </div>
-            <div className="w-full flex justify-center items-center h-full relative space-x-2.5">
-                <div className="min-w-[250px] min-h-[415px] bg-[url('/proj1.png')] bg-center rounded-xl bg-cover" />
-                <div className="space-y-4 h-full">
-                    <div className="flex space-x-4 max-h-[200px]">
-                        <div className="min-w-[250px] min-h-[200px] bg-[url('/proj2.png')] bg-center rounded-xl bg-cover" />
-                        <div className="min-w-[250px] min-h-[200px] bg-[url('/proj3.png')] bg-center rounded-xl bg-cover" />
+            <div className="w-full flex flex-col lg:flex-row justify-center items-center h-full relative space-y-2.5 lg:space-y-0 lg:space-x-5">
+                <div className="min-w-[300px] min-h-[150px] sm:min-w-[515px] sm:min-h-[200px] lg:min-w-[250px] 
+                    lg:min-h-[415px] bg-[url('/proj1.png')] bg-center rounded-xl bg-cover" />
+                <div className="space-y-2.5 h-full">
+                    <div className="flex flex-col sm:flex-row sm:space-x-4 sm:max-h-[200px] px-20 sm:px-0 space-y-2.5 sm:space-y-0">
+                        <div className="min-w-[300px] min-h-[150px] sm:min-w-[250px] sm:min-h-[200px] bg-[url('/proj2.png')] bg-center rounded-xl bg-cover" />
+                        <div className="min-w-[300px] min-h-[150px] sm:min-w-[250px] sm:min-h-[200px] bg-[url('/proj3.png')] bg-center rounded-xl bg-cover" />
                     </div>
-                    <div>
-                        <div className="min-w-[400px] min-h-[200px] bg-[url('/proj4.png')] bg-center rounded-xl bg-cover" />
+                    <div className="px-20 sm:px-0">
+                        <div className="min-w-[300px] min-h-[150px] sm:min-w-[400px] sm:min-h-[200px] bg-[url('/proj4.png')] bg-center rounded-xl bg-cover" />
                     </div>
                 </div>
             </div>
