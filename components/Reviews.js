151ccb8bdc13b18e652e1dc9797950e7768066f7
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-20 00:00:17 +0100

    suprememods

--- components/Reviews.js
+++ components/Reviews.js
@@ -1,5 +1,18 @@
+import Testimonial from "./Testimonial";
+
 export default function Reviews() {
     return(
-        <div></div>
+        <div className="w-full flex flex-col justify-center items-center space-y-4">
+            <div className="lg:w-[700px] xl:w-[600px] text-center space-y-2">
+                <h6 className="font-semibold text-pink h6">| Reviews |</h6>
+                <h1 className="font-bold text-ink h1">Testimonials From Our Clients</h1>
+                <p className="p px-2.5 sm:px-10 lg:px-5">Lorem ipsum dolor sit amet consectetur. Dui dictumst turpis tellus integer vulputate.</p>
+            </div>
+            <div className="flex space-x-5 lg:w-[850px]">
+                <Testimonial />
+                <Testimonial />
+                <Testimonial />
+            </div>
+        </div>
     )
 }
---
+++ components/Testimonial.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Testimonial(quote, image, name, job) {
+    return(
+        <div className="border border-[#EEEEEE] p-4 rounded-xl space-y-4">
+            <div>
+                <p className="p">
+                    "Dicat eripuit accumsan facilisi has cetero argumentum, 
+                    vel at fugit definitionem integre abhorreant epicurei ferri pri."
+                </p>
+            </div>
+            <div className="flex items-center space-x-4">
+                <div className="rounded-full max-w-[50px] max-h-[50px]">
+                    <Image src={"/reviewer.png"} width={50} height={50} />
+                </div>
+                <div>
+                    <p className="font-semibold">Victor</p>
+                    <p>Google</p>
+                </div>
+            </div>
+        </div>
+    )
+}
