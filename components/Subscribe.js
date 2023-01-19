87a6f115bffa70f9310e5b13b11ca6c99ea3148d
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-20 00:07:56 +0100

    suprememods

---
+++ components/Subscribe.js
@@ -0,0 +1,5 @@
+export default function Subscribe() {
+    return(
+        <div className="w-full px-4 lg:px-20 py-20 bg-pink/10"></div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import OurTeam from "../components/OurTeam";
 import Portfolio from "../components/Portfolio";
 import Reviews from "../components/Reviews";
 import Satisfied from "../components/Satisfied";
+import Subscribe from "../components/Subscribe";
 
 export default function Home() {
   return(
@@ -12,13 +13,14 @@ export default function Home() {
       <Head>
         <title>Jarvis Nu</title>
       </Head>
-      <main className="min-h-screen w-full">
+      <main className="min-h-screen w-full space-y-5">
         <Hero />
         <Satisfied />
         <Develop />
         <Portfolio />
         <OurTeam />
         <Reviews />
+        <Subscribe />
       </main>
     </div>
   )
